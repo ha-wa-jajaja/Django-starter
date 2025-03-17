d78d26559a8553b000042aaa5b7b0f19970994d6
ha-wa-jajaja <wtfuhell@gmail.com>
2025-03-17 21:16:25 +0800

    basic testings & ignore

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract y from x and return result."""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+# SimpleTestCase: No DB interaction
+# TestCase: DB interaction
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 3)
+
+        self.assertEqual(res, 7)

8633e528d0548b98832165fbf74adbd80768cf66
ha-wa-jajaja <wtfuhell@gmail.com>
2025-04-04 12:31:06 +0800

    Course 9 & 10

--- app/bugbytes/urls.py
+++ app/bugbytes/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.product_list),
-    path("products/info/", views.product_info),
+    path("products/", views.ProductListCreateAPIView.as_view()),
+    path("products/info/", views.ProductsInfoAPIView.as_view()),
     path("products/<int:pk>/", views.product_detail),
     path("product-view/<int:product_id>", views.ProductDetailAPIView.as_view()),
     path("orders/", views.order_list),
--- app/bugbytes/views.py
+++ app/bugbytes/views.py
@@ -3,16 +3,17 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Order, Product
 from .serializers import OrderSerializer, ProductSerializer, ProductsInfoSerializer
 
 
-@api_view(["GET"])
-def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+# NOTE: DRF provides set of custom generic views to handle common tasks
+# In this case, ListCreateAPIView to handle both listing and creating products
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
 @api_view(["GET"])
@@ -22,6 +23,7 @@ def product_detail(request, pk):
     return Response(serializer.data)
 
 
+# NOTE: Django also allows to use function-based views
 @api_view(["GET"])
 def order_list(request):
     orders = Order.objects.all()
@@ -29,23 +31,29 @@ def order_list(request):
     return Response(serializer.data)
 
 
-@api_view(["GET"])
-def product_info(request):
-    products = Product.objects.all()
-    serializer = ProductsInfoSerializer(
-        {
-            "products": products,
-            "count": len(products),
-            # NOTE:
-            # .aggregate() is a Django QuerySet method that performs a calculation over all objects and returns a dictionary
-            # max_price=Max('price') is specifying what we want to calculate
-            # ['max_price'] is accessing the result of the aggregation by its key
-            # aggregate() transforms to a SQL command to calculate in DB level, making it more efficient
-            "max_price": products.aggregate(max_price=Max("price"))["max_price"],
-            "min_price": products.aggregate(min_price=Min("price"))["min_price"],
-        }
-    )
-    return Response(serializer.data)
+# NOTE:
+# APIView is a class-based view provided by Django REST framework
+# Allows more flexibility and customization compared to function-based views
+# It provides methods like get(), post(), put(), delete() to handle different HTTP methods
+# It also provides access to request and response objects,
+# making it easier to work with API requests and responses
+class ProductsInfoAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductsInfoSerializer(
+            {
+                "products": products,
+                "count": len(products),
+                # NOTE:
+                # .aggregate() is a Django QuerySet method that performs a calculation over all objects and returns a dictionary
+                # max_price=Max('price') is specifying what we want to calculate
+                # ['max_price'] is accessing the result of the aggregation by its key
+                # aggregate() transforms to a SQL command to calculate in DB level, making it more efficient
+                "max_price": products.aggregate(max_price=Max("price"))["max_price"],
+                "min_price": products.aggregate(min_price=Min("price"))["min_price"],
+            }
+        )
+        return Response(serializer.data)
 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
